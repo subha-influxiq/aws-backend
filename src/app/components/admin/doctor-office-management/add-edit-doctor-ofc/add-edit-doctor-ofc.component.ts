14b59f2cca2e6a6e9279df77c4ee48f85132cf26
souresh <souresh.influxiq@gmail.com>
2019-11-25 17:54:25 +0530

    updates

--- src/app/components/admin/doctor-office-management/add-edit-doctor-ofc/add-edit-doctor-ofc.component.ts
+++ src/app/components/admin/doctor-office-management/add-edit-doctor-ofc/add-edit-doctor-ofc.component.ts
@@ -144,12 +144,17 @@ export class AddEditDoctorOfcComponent implements OnInit {
   allStateCityData() {
     this.httpService.getSiteSettingData("./assets/data-set/state.json").subscribe(response => {
       this.states = response;
-      this.getResolveData();
+      if(this.params_id){
+        this.getResolveData();
+      }
+      
     });
 
     this.httpService.getSiteSettingData("./assets/data-set/city.json").subscribe(response => {
       this.allCities = response;
-      this.getResolveData();
+      if(this.params_id){
+        this.getResolveData();
+      }
     });
   }
   /**for getting all states & cities  function end here**/
