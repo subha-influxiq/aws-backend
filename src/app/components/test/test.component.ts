84fdb87545e8e39d5958b2bd75f7babdf33f890a
Subha Prasad <subha.influxiq@gmail.com>
2020-02-28 16:09:11 +0530

    updated

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpServiceService } from '../../services/http-service.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CommonFunction } from '../../class/common/common-function';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
@@ -18,6 +16,7 @@ export class TestComponent implements OnInit {
   public resolveData: any;
   public dataSource: any;
   public displayedColumns:any;
+  public lastsearchcond:any={};
   public displaycolvals:any = [];
 
   public search: any = {
@@ -32,15 +31,16 @@ export class TestComponent implements OnInit {
   };
 
   public sort_val: any;
+  public sort_item: any = {};
   public sort_type: any;
   public total_count: any = 0;
 
   constructor(private router: Router, public cookieService: CookieService, private http: HttpServiceService, public activatedRoute: ActivatedRoute, public commonFunction: CommonFunction) {
-    this.displayedColumns=[
-      {key:"name",value:'Name'},
-      {key:"DOB",value:'Date of Birth'},
-      {key:"DOA",value:'DOA'},
-      {key:"roll",value:'Role'},
+    this.displayedColumns = [
+      {key:"name", value:'Name'},
+      {key:"DOB", value:'Date of Birth'},
+      {key:"DOA", value:'DOA'},
+      {key:"roll", value:'Roll'},
       {key:"class", value: "Class"},
       {key:"address", value: "Address"},
       {key:"parent_phone", value: "Parent Phone"},
@@ -50,7 +50,7 @@ export class TestComponent implements OnInit {
       {key:"weight", value: "Weight"},
       {key:"height", value: "Height"}
     ];
-    let data: any = Array.from(this.displayedColumns, x => x.key);
+    let data: any = Array.from(this.displayedColumns, (x:any) => x.key);
     this.displaycolvals = ['#'];
     this.displaycolvals = this.displaycolvals.concat(data);
 
@@ -67,17 +67,45 @@ export class TestComponent implements OnInit {
   }
 
   getPageData() {
+    let searchcond:any = {};
+    let serachval = this.search;
+    let searcharr = Object.keys(serachval).map(function (key) { 
+      // Using Number() to convert key to number type 
+      // Using obj[key] to retrieve key value 
+      return {key:key, val:serachval[key]};   
+  });
+
+  console.log('search arr',searcharr);
+  for(let k in searcharr){
+
+    if(searcharr[k].val!=null && searcharr[k].val!=''){
+      searchcond[searcharr[k].key]={$regex:searcharr[k].val};
+
+    }
+  }
+  searchcond={$and:[searchcond]}; 
+  this.lastsearchcond=searchcond;
+  console.log(searchcond,'cond'); 
+  //return;
+    //for(lel key in this.search){}
     let repostSignCond: any = {
       "source": "data_pece",
       "condition": {},
-      "skip": parseInt(this.page.page_no) * parseInt(this.page.page_count),
+      "skip": (parseInt(this.page.page_no)-1) * parseInt(this.page.page_count),
       "limit": parseInt(this.page.page_count),
+      "search": searchcond,
       "sort_val": this.sort_val,
       "sort_type": this.sort_type
     };
 
+    
+
+    //console.log('repostSignCond.search',repostSignCond.search);
+    //return;
     this.http.httpViaPost('test-datalist', repostSignCond).subscribe((response) => {
       if(response.status == 'success') {
+        this.total_count=0;
+        this.getPageCount();
         this.dataSource = new MatTableDataSource(response.data);
       } else {
         console.log(response);
@@ -86,8 +114,10 @@ export class TestComponent implements OnInit {
   }
 
   sortPageData(item: any) {
+    this.sort_item.header = item; 
     if(item != this.sort_val) {
       this.sort_val = item;
+      // this.sort_type.header = item;
       this.sort_type = 'asc';
       this.getPageData();
     } else {
@@ -111,11 +141,12 @@ export class TestComponent implements OnInit {
     }
     this.getPageData();
   }
-
+  
   getPageCount() {
     let repostSignCond: any = {
       sort_val: "doa",
-      sort_type: "desc"
+      sort_type: "desc",
+      search:this.lastsearchcond
     };
 
 
