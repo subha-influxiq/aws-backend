6a3d7ef0755373929243b7e28dda076eaf1827da
subrata04 <subrata.influxiq@gmail.com>
2020-03-20 19:02:04 +0530

    subrata

--- src/app/components/sales-person/sales-person-dashboard/sales-person-dashboard.component.ts
+++ src/app/components/sales-person/sales-person-dashboard/sales-person-dashboard.component.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+
+const ELEMENT_DATA:any = [
+  {no:'1', doctors_name: 'binay', email: 'Hydrogen', phone: 1.0079, NPI: 'H', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'badal', email: 'Helium', phone: 4.0026, NPI: 'He', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'dinesh', email: 'Lithium', phone: 6.941, NPI: 'Li', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'corona', email: 'Beryllium', phone: 9.0122, NPI: 'Be', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'Virus', email: 'Boron', phone: 10.811, NPI: 'B', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'came', email: 'Carbon', phone: 12.0107, NPI: 'C', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'gone', email: 'Nitrogen', phone: 14.0067, NPI: 'N', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'binay', email: 'Hydrogen', phone: 1.0079, NPI: 'H', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'badal', email: 'Helium', phone: 4.0026, NPI: 'He', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'dinesh', email: 'Lithium', phone: 6.941, NPI: 'Li', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'corona', email: 'Beryllium', phone: 9.0122, NPI: 'Be', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'Virus', email: 'Boron', phone: 10.811, NPI: 'B', addon:'10/10/2020', address:'corona home'},
+  {no:'1', doctors_name: 'came', email: 'Carbon', phone: 12.0107, NPI: 'C', addon:'10/10/2020', address:'corona home'},
+
+]
 
 @Component({
   selector: 'app-sales-person-dashboard',
@@ -6,10 +24,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-dashboard.component.css']
 })
 export class SalesPersonDashboardComponent implements OnInit {
+  displayedColumns: string[] = [ 'no', 'doctors_name', 'email', 'phone', 'NPI', 'addon', 'address'];
 
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   constructor() { }
 
   ngOnInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
 }
