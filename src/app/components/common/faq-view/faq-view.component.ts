985b215367655d6a364a77be36ad1f153bf4adae
Subha Prasad <subha.influxiq@gmail.com>
2020-03-24 11:55:33 +0530

    updated faq page

--- src/app/components/common/faq-view/faq-view.component.ts
+++ src/app/components/common/faq-view/faq-view.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonFunction } from '../../../class/common/common-function';
+import { Router, ActivatedRoute ,ActivatedRouteSnapshot} from '@angular/router';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource, MatSort } from '@angular/material';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpServiceService } from '../../../services/http-service.service';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-faq-view',
@@ -7,9 +16,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqViewComponent implements OnInit {
 
-  constructor() { }
+  public loginUserData: any = {};
+  public allResolveData: any;
+
+  constructor(public http: HttpClient, public commonFunction: CommonFunction, public activatedRoute: ActivatedRoute, 
+    public cookieService: CookieService, public httpService: HttpServiceService, public deviceService: DeviceDetectorService,
+    public matSnackBar: MatSnackBar) {
+    
+      /* Get and set login User Data */
+    this.loginUserData["user_details"] = JSON.parse(this.cookieService.get('user_details'));
+    this.loginUserData["jwtToken"] = this.cookieService.get('jwtToken');
+  }
 
   ngOnInit() {
+    this.activatedRoute.data.subscribe(resolveData => {
+      this.allResolveData = resolveData.faqData.data;
+    });
   }
 
 }
