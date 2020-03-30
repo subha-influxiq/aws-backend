37789a82cee56caaafd22089177220851ce4bc39
Subha Prasad <subha.influxiq@gmail.com>
2020-03-30 22:24:31 +0530

    updated

---
+++ src/app/components/tech/manage-calender/manage-schedule/cal-home/cal-home.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpServiceService } from '../../../../../services/http-service.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CommonFunction } from '../../../../../class/common/common-function';
+import { MatTableDataSource } from '@angular/material/table';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { MatSnackBar } from '@angular/material';
+import { environment } from '../../../../../../environments/environment';
+
+@Component({
+  selector: 'app-cal-home',
+  templateUrl: './cal-home.component.html',
+  styleUrls: ['./cal-home.component.css']
+})
+export class CalHomeComponent implements OnInit {
+
+  public configData: any = {
+    jwtToken: "",
+    baseUrl: environment.apiBaseUrl,
+    endPoint: {
+      add: "add-or-update-event-data",
+      datalist: "cal-datalist",
+      deleteEvent: "cal-delete-single-event",
+      viewEventSlots: "cal-view-event-eventdayarr",
+      search: "cal-search",
+      countSlot: "cal-count-slot"
+    },
+    urls: {
+      view: "calendar-management",
+      viewSlotUser: "tech/manage-calender/manage-sehedule/view-slot-user",
+      eventListing: "tech/manage-calender/manage-sehedule/event-listing",
+      add: "tech/manage-calender/manage-sehedule/create-slot",
+      edit: "",
+      // googleSync: "calendar-management/sync-with-google",
+      googleSync: environment.googleSyncApi
+    },
+    timeZone: [
+      { text: 'Alaska Standard Time', value: '-08:00|America/Anchorage' },
+      { text: 'Pacific Standard Time', value: '-07:00|America/Los_Angeles' },
+      { text: 'Mountain Standard Time(GMT-06:00)', value: '-06:00|America/Denver' },
+      { text: 'Mountain Standard Time(GMT-07:00) (no DST)', value: '-07:00|America/Phoenix' },
+      { text: 'Central Standard Time', value: '-05:00|America/Chicago' },
+      { text: 'Eastern Standard Time', value: '-04:00|America/New_York' },
+      { text: 'Hawaii Standard Time', value: '-10:00|Pacific/Honolulu' }
+    ],
+    eventType: [
+      { text: "Admin Meetings", value: 1 },
+      { text: "Type 2", value: 2 },
+      { text: "Type 3", value: 3 },
+      { text: "Type 3", value: 4 }
+    ],
+    responseData: ""
+  };
+
+  constructor(private http: HttpClient, public activatedRoute: ActivatedRoute, public cookieService: CookieService, public snackBar: MatSnackBar) {
+  }
+
+  ngOnInit() {
+    if (this.cookieService.check('jwtToken')) {
+      this.configData.jwtToken = this.cookieService.get('jwtToken');
+      this.activatedRoute.data.forEach((data) => {
+        this.configData.responseData = data.eventdayarrData.data;
+        console.log('responseData: ', data.eventdayarrData.data);
+      });
+    } else {
+      this.openSnackBar("Token not found", null);
+    }
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 3000,
+    });
+  }
+
+  /* call api via post method */
+  httpViaPost(endpoint, jsonData): Observable<any> {
+    /* set common header */
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': jsonData.token
+      })
+    };
+    return this.http.post(endpoint, jsonData);
+  }
+
+}
