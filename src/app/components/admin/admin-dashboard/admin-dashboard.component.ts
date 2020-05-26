d64a9cbccb16ec7d0ce8bf3f20e89503fb18d8fc
Subha Prasad <subha.influxiq@gmail.com>
2020-05-26 17:46:18 +0530

    updated

--- src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
@@ -100,7 +100,7 @@ export class AdminDashboardComponent implements OnInit {
   public deleteEndpoint: any = "deletesingledata";
   public apiUrl: any = environment.apiBaseUrl;
   public tableName: any = "data_pece";
-  public datacollection: any = 'getbillerlistdata';
+  public datacollection: any = 'getPatientlistdata';
 
   public sortdata: any = {
     "type": 'desc',
