3fa4f9b9209048fcbe047736ae23984560265659
rohitinfluxiq <rohit.influxiq@gmail.com>
2020-06-04 12:19:07 +0530

    fix search

--- src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
@@ -63,7 +63,7 @@ export class AdminDashboardComponent implements OnInit {
   public userData: any;
   public libdata: any = {
     basecondition: { status: { $gt:7 ,$lt:11 } },
-    updateendpoint: 'statusupdate',
+    updateendpoint: 'status-update',
     custombuttons: [
       {
         label: "View Report",
@@ -204,7 +204,7 @@ export class AdminDashboardComponent implements OnInit {
   public search_settings: any =
     {
       
-      selectsearch: [{ label: 'Search By Report Type', field: 'report_file_type', values: this.report_type } , { label: 'Search By Parent Type', field: 'parent_type', values: this.parent_type },{label: "Search By Doctor", field: 'doc_name_search', values:this.authval },{label: "Search By Tech", field: 'tech_name_search', values:this.techval },{label: "Search By Doctor Office", field: 'author_search', values:this.docofficeval },{label: "Search By Parent Name", field: 'patient_name', values:this.parentnameval },{label: "Search By Doctor City", field: 'author_search', values:this.doctorcity },{label: "Search By Doctor State", field: 'author_search', values:this.doctorstate },{label: "Search By Parent City", field: 'author_search', values:this.patientcity },{label: "Search By Parent State", field: 'author_search', values:this.patientstate }],
+      selectsearch: [{ label: 'Search By Report Type', field: 'report_file_type', values: this.report_type } , { label: 'Search By Parent Type', field: 'parent_type', values: this.parent_type },{label: "Search By Doctor", field: 'doc_name_search', values:this.authval },{label: "Search By Tech", field: 'tech_name_search', values:this.techval },{label: "Search By Doctor Office", field: 'doctor_ofiice_name_search', values:this.docofficeval },{label: "Search By Parent Name", field: 'parent_name_search', values:this.parentnameval },{label: "Search By Doctor City", field: 'doctor_city_search', values:this.doctorcity },{label: "Search By Doctor State", field: 'doctor_state_search', values:this.doctorstate },{label: "Search By Parent City", field: 'patient_state_search', values:this.patientcity },{label: "Search By Parent State", field: 'patient_city_search', values:this.patientstate }],
       datesearch: [{ startdatelabel: "Start Date", enddatelabel: "End Date", submit: "Search", field: "created_at_datetime" }], 
       // textsearch: [{ label: "Search By Name", field: 'name_search' },
       // { label: "Search By E-Mail", field: 'email' }, { label: "Search By Parent Name", field: 'parent_search' }, { label: "Search By Company Name", field: 'company_search' }],
@@ -299,32 +299,101 @@ export class AdminDashboardComponent implements OnInit {
       }
       for(var i in response.res) {
         if(response.res[i].parent_name_search !="") {
-        this.parentnameval.push({name:response.res[i].parent_name,val:response.res[i].parent_name_search})
+          for(var j in this.parentnameval) {
+            if(response.res[i].parent_namesearch == this.parentnameval[j].name) {
+              start = true;
+            }
+          }
+          count ++;
+          if(count == 1 && start ==false) {
+            this.parentnameval.push({name:response.res[i].parent_namesearch,val:response.res[i].parent_name_search})
+          }
+          start = false;
+          count = 0;
+        
         }
       }
       for(var i in response.res) {
         if(response.res[i].doctor_state_search !="") {
-        this.doctorstate.push({name:response.res[i].doctor_state,val:response.res[i].doctor_state_search})
+          for(var j in this.doctorstate) {
+            if(response.res[i].doctor_state == this.doctorstate[j].name) {
+              start = true;
+            }
+          }
+          count ++;
+          if(count == 1 && start ==false) {
+            this.doctorstate.push({name:response.res[i].doctor_state,val:response.res[i].doctor_state_search})
+          }
+          start = false;
+          count = 0;
+        
+        
         }
       }
       for(var i in response.res) {
         if(response.res[i].doctor_city_search !="") {
-        this.doctorcity.push({name:response.res[i].doctor_city,val:response.res[i].doctor_city_search})
+          for(var j in this.doctorcity) {
+            if(response.res[i].doctor_city == this.doctorcity[j].name) {
+              start = true;
+            }
+          }
+          count ++;
+          if(count == 1 && start ==false) {
+            this.doctorcity.push({name:response.res[i].doctor_city,val:response.res[i].doctor_city_search})
+          }
+          start = false;
+          count = 0;
+        
+        
         }
       }
       for(var i in response.res) {
         if(response.res[i].patient_city_search !="") {
-        this.patientcity.push({name:response.res[i].patient_city,val:response.res[i].patient_city_search})
+          for(var j in this.patientcity) {
+            if(response.res[i].patient_city == this.patientcity[j].name) {
+              start = true;
+            }
+          }
+          count ++;
+          if(count == 1 && start ==false) {
+            this.patientcity.push({name:response.res[i].patient_city,val:response.res[i].patient_city_search})
+          }
+          start = false;
+          count = 0;
+        
+        
         }
       }
       for(var i in response.res) {
         if(response.res[i].patient_state_search !="") {
-        this.patientstate.push({name:response.res[i].patient_state,val:response.res[i].patient_state_search })
+          for(var j in this.patientstate) {
+            if(response.res[i].patient_state == this.patientstate[j].name) {
+              start = true;
+            }
+          }
+          count ++;
+          if(count == 1 && start ==false) {
+            this.patientstate.push({name:response.res[i].patient_state,val:response.res[i].patient_state_search })
+          }
+          start = false;
+          count = 0;
+        
+        
         }
       }
       for(var i in response.res) {
         if(response.res[i].doctor_ofiice_name_search !="") {
-        this.docofficeval.push({name:response.res[i].doctor_ofiice_name,val:response.res[i].doctor_ofiice_name_search })
+          for(var j in this.docofficeval) {
+            if(response.res[i].doctor_ofiice_namesearch == this.docofficeval[j].name) {
+              start = true;
+            }
+          }
+          count++;
+          if (count == 1 && start == false) { 
+            this.docofficeval.push({name:response.res[i].doctor_ofiice_namesearch,val:response.res[i].doctor_ofiice_name_search }) 
+        } 
+        start = false; 
+        count = 0;
         }
       }
     }, error => {
