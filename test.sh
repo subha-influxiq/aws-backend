1658464b086b46d6d35e5ba54743908c5ea7ca4d
Subha Prasad <workplace.subha@gmail.com>
2020-06-11 10:24:37 +0530

    updated

--- test.sh
+++ test.sh
@@ -1,6 +1,49 @@
-ng build --prod
+#!/bin/sh
 
-cd dist/
+GitProcessStart() {
+	git status
 
-aws --profile default s3 sync browser s3://testbedpece.influxiq.com --acl public-read  --cache-control max-age=0
+	git add .
+}
+
+GitCommitMsg () {
+	read -p 'Enter commit message: ' commitMsg
+
+	if [ $commitMsg ] 
+	then
+   		git commit -m $commitMsg
+	else
+   		echo "Error: Please enter your commit message."
+   		GitCommitMsg
+	fi
+}
+
+GitBranchName () {
+	read -p 'Enter git branch name: ' gitBranch
+
+	if [ $gitBranch ] 
+	then
+		git pull origin $gitBranch
+   		return $gitBranch
+	else
+   		echo "Error: Please enter your branch name."
+   		GitBranchName
+	fi
+}
+
+GitProcessStart
+GitCommitMsg
+GitBranchName
+gitBranch = $?
+
+read -p 'Conflict record ?? (y/n): ' conflictKey
+
+if [ $conflictKey == 'y' ] || [ $conflictKey == 'Y' ] 
+then
+	GitProcessStart
+else
+	git push origin master
+fi
+
+echo "Successfully done."
 
