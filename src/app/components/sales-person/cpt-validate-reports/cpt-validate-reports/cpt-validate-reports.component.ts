2d4b8776014adbd02e9b20c8e16e912eb08730ab
rohitinfluxiq <rohit.influxiq@gmail.com>
2020-06-18 16:03:13 +0530

    update sales person

---
+++ src/app/components/sales-person/cpt-validate-reports/cpt-validate-reports/cpt-validate-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cpt-validate-reports',
+  templateUrl: './cpt-validate-reports.component.html',
+  styleUrls: ['./cpt-validate-reports.component.css']
+})
+export class CptValidateReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/sales-person/sales-person-dashboard/sales-person-dashboard.component.ts
+++ src/app/components/sales-person/sales-person-dashboard/sales-person-dashboard.component.ts
@@ -1,23 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+import { HttpServiceService } from '../../../services/http-service.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router,ActivatedRoute } from '@angular/router';
 import { environment } from '../../../../environments/environment';
 
-// const ELEMENT_DATA:any = [
-//   {no:'1', doctors_name: 'binay', email: 'Hydrogen', phone: 1.0079, NPI: 'H', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'badal', email: 'Helium', phone: 4.0026, NPI: 'He', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'dinesh', email: 'Lithium', phone: 6.941, NPI: 'Li', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'corona', email: 'Beryllium', phone: 9.0122, NPI: 'Be', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'Virus', email: 'Boron', phone: 10.811, NPI: 'B', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'came', email: 'Carbon', phone: 12.0107, NPI: 'C', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'gone', email: 'Nitrogen', phone: 14.0067, NPI: 'N', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'binay', email: 'Hydrogen', phone: 1.0079, NPI: 'H', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'badal', email: 'Helium', phone: 4.0026, NPI: 'He', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'dinesh', email: 'Lithium', phone: 6.941, NPI: 'Li', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'corona', email: 'Beryllium', phone: 9.0122, NPI: 'Be', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'Virus', email: 'Boron', phone: 10.811, NPI: 'B', addon:'10/10/2020', address:'corona home'},
-//   {no:'1', doctors_name: 'came', email: 'Carbon', phone: 12.0107, NPI: 'C', addon:'10/10/2020', address:'corona home'},
-
-// ]
 
 @Component({
   selector: 'app-sales-person-dashboard',
@@ -27,24 +13,156 @@ import { environment } from '../../../../environments/environment';
 export class SalesPersonDashboardComponent implements OnInit {
   displayedColumns: string[] = [ 'no', 'doctors_name', 'email', 'phone', 'NPI', 'addon', 'address'];
 
-  public shareDetails: any = {
-    baseUrl: environment.doctorSignUpBaseUrl,
-    userId: "5daffd4aba196c6e298ef119"
+  public userData: any={
+  };
+  public shareDetails:any = {};
+  // public cookieUserallData: any = JSON.parse(this.cookieService.get('user_details'))
+  public docData_count:any=0;
+  public  docData: any = [];
+  public datasource: any;
+  public docData_skip: any = [
+    "_id",
+    "fax",
+    "address",
+    "zip",
+    "city",
+    "state",
+    "user_type",
+    "tech_id",
+    "biller_id",
+    "doctors_office_id",
+    "diagnostic_admin_id",
+    "taxo_list",
+    "password",
+    "created_at",
+    "id",
+    "name_search",
+    "updated_at",
+    "doctor_signature",
+    "distributor_id",
+    "diagnostic_admin_id",
+    "doctorgroup_id"
+  ];
+  public docData_modify_header: any = {
+    firstname: "First Name",
+    lastname: "Last Name",
+    practice_name: "Practice Name",
+    parent_name: "Parent Name",
+    parent_type: "Parent Type",
+    npi: "NPI#",
+    email: "Email",
+    phone: "Phone Number",
+    status: "Status",
+    created_date :"Created Date"
   };
 
-  // dataSource = new MatTableDataSource(ELEMENT_DATA);
+  public previewModal_skip: any = [
+    "_id",
+    "user_type",
+    "tech_id",
+    "biller_id",
+    "doctors_office_id",
+    "taxo_list",
+    "password",
+    "created_at",
+    "id",
+    "name_search",
+    "updated_at"
+  ];
+  public tableName: any = 'users';
+  public UpdateEndpoint: any = "addorupdatedata";
+  public deleteEndpoint: any = "deletesingledata";
+  public user_cookie: any;
+  public searchingEndpoint:any="datalist";
+  public searchSourceName:any="data_doctor_list"
+  public editUrl:any = 'sales-person/doctor-management/edit';
+  public apiUrl:any;
+  public datacollection: any='getdoctorlistdata';
+  public data:any;
+  public field:any;
+  public fetch:any;
+  public libdata:any={
+    basecondition: "",
+    updateendpoint:'statusupdate',
+    hidedeletemany: true,
+    // hideeditbutton:true,// all these button options are optional not mandatory
+    //hidedeletebutton:true,
+    //hideviewbutton:false,
+    //hidestatustogglebutton:true,
+    // hideaction:true,
+    tableheaders:['firstname','lastname','email','phone','practice_name','npi','status','created_date',], //not required
+}
+  public sortdata:any={
+    "type":'desc',
+    "field":'firstname',
+    "options":['firstname','email','practice_name','npi','status','created_date']
+ };
+ public limitcond:any={
+  "limit":10,
+  "skip":0,
+  "pagecount":1
+};
+  public status: any = [{ val: 1, 'name': 'Active' }, { val: 0, 'name': 'Inactive' }];
+  public search_settings: any =
+    {
+      selectsearch: [{ label: 'Search By Status', field: 'status', values: this.status }],
+      textsearch: [{ label: "Search By Name", field: 'name_search' },
+      // {label:"Search by Taxonomy",field:'taxo_list'},
+      { label: "Search By E-Mail", field: 'email' },{ label: "Search By NPI", field: 'npi' }]
+    };
+  // ====================================================================
 
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  constructor() { }
+  constructor(private http: HttpServiceService, private cookieService: CookieService, 
+    private router: Router,public activatedRoute : ActivatedRoute) {
+
+    this.user_cookie = cookieService.get('jwtToken');
+    let allData = cookieService.getAll();
+    this.userData = JSON.parse(allData.user_details);
+
+    this.libdata.basecondition = {parent_id:this.userData._id};
+    this.shareDetails={
+      baseUrl: environment.doctorSignUpBaseUrl,
+      userId: this.userData._id
+    };
+    // console.log('libdata',this.libdata);
+
+    this.apiUrl = http.baseUrl;
+  }
 
   ngOnInit() {
-    // this.dataSource.sort = this.sort;
-    // this.dataSource.paginator = this.paginator;
-  }
-
-  pagination(status: any = null) {
-
-  }
+    this.datasource = '';
+    let endpoint='getdoctorlistdata';
+    let endpointc='getdoctorlistdata-count';
+    let data:any={
+        "condition":{
+            "limit":10,
+            "skip":0
+        },
+    sort:{
+        "type":'desc',
+        "field":'firstname'
+    },
+    data:{
+      parent_id:this.userData._id
+    }
+    }
+        this.http.httpViaPost(endpointc, data).subscribe((res:any) => {
+            // console.log('in constructor');
+            // console.log(result);
+            this.docData_count =res.count;
+            //console.warn('blogData c',res);
+ 
+        }, error => {
+            console.log('Oooops!');
+        });
+ 
+        this.http.httpViaPost(endpoint,data).subscribe((res:any) => {
+           
+            this.docData =res.results.res;
+ 
+        }, error => {
+            console.log('Oooops!');
+        });
+      }
 
 }
