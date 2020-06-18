18fd8e8e7b18b5b1dd3cbf590b50ba19b2802b51
rohitinfluxiq <rohit.influxiq@gmail.com>
2020-06-18 13:21:28 +0530

    fixing

--- src/app/components/sales-person/sales-person-dashboard/sales-person-dashboard.component.ts
+++ src/app/components/sales-person/sales-person-dashboard/sales-person-dashboard.component.ts
@@ -2,22 +2,22 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { environment } from '../../../../environments/environment';
 
-const ELEMENT_DATA:any = [
-  {no:'1', doctors_name: 'binay', email: 'Hydrogen', phone: 1.0079, NPI: 'H', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'badal', email: 'Helium', phone: 4.0026, NPI: 'He', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'dinesh', email: 'Lithium', phone: 6.941, NPI: 'Li', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'corona', email: 'Beryllium', phone: 9.0122, NPI: 'Be', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'Virus', email: 'Boron', phone: 10.811, NPI: 'B', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'came', email: 'Carbon', phone: 12.0107, NPI: 'C', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'gone', email: 'Nitrogen', phone: 14.0067, NPI: 'N', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'binay', email: 'Hydrogen', phone: 1.0079, NPI: 'H', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'badal', email: 'Helium', phone: 4.0026, NPI: 'He', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'dinesh', email: 'Lithium', phone: 6.941, NPI: 'Li', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'corona', email: 'Beryllium', phone: 9.0122, NPI: 'Be', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'Virus', email: 'Boron', phone: 10.811, NPI: 'B', addon:'10/10/2020', address:'corona home'},
-  {no:'1', doctors_name: 'came', email: 'Carbon', phone: 12.0107, NPI: 'C', addon:'10/10/2020', address:'corona home'},
+// const ELEMENT_DATA:any = [
+//   {no:'1', doctors_name: 'binay', email: 'Hydrogen', phone: 1.0079, NPI: 'H', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'badal', email: 'Helium', phone: 4.0026, NPI: 'He', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'dinesh', email: 'Lithium', phone: 6.941, NPI: 'Li', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'corona', email: 'Beryllium', phone: 9.0122, NPI: 'Be', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'Virus', email: 'Boron', phone: 10.811, NPI: 'B', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'came', email: 'Carbon', phone: 12.0107, NPI: 'C', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'gone', email: 'Nitrogen', phone: 14.0067, NPI: 'N', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'binay', email: 'Hydrogen', phone: 1.0079, NPI: 'H', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'badal', email: 'Helium', phone: 4.0026, NPI: 'He', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'dinesh', email: 'Lithium', phone: 6.941, NPI: 'Li', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'corona', email: 'Beryllium', phone: 9.0122, NPI: 'Be', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'Virus', email: 'Boron', phone: 10.811, NPI: 'B', addon:'10/10/2020', address:'corona home'},
+//   {no:'1', doctors_name: 'came', email: 'Carbon', phone: 12.0107, NPI: 'C', addon:'10/10/2020', address:'corona home'},
 
-]
+// ]
 
 @Component({
   selector: 'app-sales-person-dashboard',
@@ -32,15 +32,15 @@ export class SalesPersonDashboardComponent implements OnInit {
     userId: "5daffd4aba196c6e298ef119"
   };
 
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  // dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   constructor() { }
 
   ngOnInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
+    // this.dataSource.paginator = this.paginator;
   }
 
   pagination(status: any = null) {
