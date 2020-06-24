68486087dbcbdcf521c1880e19f7c7b783445097
Subha Prasad <workplace.subha@gmail.com>
2020-06-24 09:56:12 +0530

    updated

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1091,6 +1091,19 @@ const routes: Routes = [
       endpoint: 'list-booked-events'
     }
   },
+  {
+    path: 'admin/booked-appoinments/:tab_flag',
+    component: BookedAppoinmentsComponent,
+    canActivate: [AuthguardService],
+    // resolve: { bookedEventList: ResolveService },
+    data: {
+      requestcondition: {
+        source: 'google-events',
+        condition: {}
+      },
+      endpoint: 'list-booked-events'
+    }
+  },
 
   /* Faq */
   {
@@ -1133,7 +1146,7 @@ const routes: Routes = [
           source: 'data_pece',
           condition: {}
         },
-        endpoint: 'diagnostic-admin-dashboard'
+        endpoint: 'doctor-group-dashboard'
       },
     },
     {
