23f14334e4de9fff36b7c0693cb6ab23e41e2b65
Subha Prasad <workplace.subha@gmail.com>
2020-06-30 15:38:06 +0530

    updated

--- src/app/components/admin/admin-dashboard/view-all-button-list/view-all-button-list.component.ts
+++ src/app/components/admin/admin-dashboard/view-all-button-list/view-all-button-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener, inject, Input } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpServiceService } from '../../../../services/http-service.service';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -16,11 +16,20 @@ import { from } from 'rxjs';
 const moment = momentImported;
 
 @Component({
-  selector: 'app-approved-patient-reports',
-  templateUrl: './approved-patient-reports.component.html',
-  styleUrls: ['./approved-patient-reports.component.css']
+  selector: 'app-view-all-button-list',
+  templateUrl: './view-all-button-list.component.html',
+  styleUrls: ['./view-all-button-list.component.css']
 })
-export class ApprovedPatientReportsComponent implements OnInit {
+
+export class ViewAllButtonListComponent implements OnInit {
+
+  public viewFlagData: string = "";
+  @Input()
+  set viewFlag(viewFlagData: any) {
+    this.viewFlagData = viewFlagData;
+
+    this.viewReportProcessData(this.viewFlagData);
+  }
 
   public loginUserData: any = {};
   public jwtToken: string = "";
@@ -61,7 +70,7 @@ export class ApprovedPatientReportsComponent implements OnInit {
   public editUrl: any = "admin/biller-management/edit";
   public userData: any;
   public libdata: any = {
-    basecondition: { status: { $gt: 10, $lt: 14 } },
+    basecondition: { status: { $gt: 10, $lt: 14 }, job_tickets_details: { $exists: true } },
     updateendpoint: '',
     custombuttons: [
       {
@@ -71,20 +80,13 @@ export class ApprovedPatientReportsComponent implements OnInit {
         param: ['_id'],
       },
       {
-        label: "Download Report",
-        link: "https://s3.us-east-2.amazonaws.com/crmfiles.influxhostserver/reports",
-        type: 'externallink',
-        paramtype: 'angular',
-        param: ['download_file_name']
-      },
-      {
         label: "Tech Details",
         type: 'action',
         datatype: 'api',
         endpoint: 'get-tech-details',
         datafields: ['first name', 'last name', 'email', 'phone', 'address', 'city', 'state', 'zip'],
         param: 'id',
-        headermessage: 'Tech Information',
+        headermessage: 'Tech Info',
       },
       {
         label: "View Codes",
@@ -100,16 +102,16 @@ export class ApprovedPatientReportsComponent implements OnInit {
         type: 'action',
         datatype: 'api',
         endpoint: 'get-doctor-details',
-        datafields: ['firstname', 'lastname', 'email', 'fax', 'Practice Name', 'NPI', 'phone', 'address', 'city', 'state', 'zip'],
+        datafields: ['firstname', 'lastname', 'email', 'fax', 'practice_name', 'npi', 'phone', 'address', 'city', 'state', 'zip'],
         param: 'id',
-        headermessage: 'Doctor Information',
+        headermessage: 'Doctor Info',
       },
       {
         label: "Doctor Office Details",
         type: 'action',
         datatype: 'api',
         endpoint: 'get-doctor-office-details',
-        datafields: ['centername', 'firstname', 'lastname', 'email', 'phone', 'address', 'city', 'state', 'zip'],
+        datafields: ['center name', 'first name', 'last name', 'email', 'phone', 'address', 'city', 'state', 'zip'],
         param: 'id',
         headermessage: 'Doctor Office Info',
       },
@@ -119,6 +121,8 @@ export class ApprovedPatientReportsComponent implements OnInit {
         datatype: 'api',
         endpoint: 'get-parent-details',
         datafields: ['Parent Name', 'Contact Person', 'email', 'phone', 'address', 'city', 'state', 'zip'],
+        cond: "parent_check_flag",
+        condval: 1,
         param: 'id',
         headermessage: 'Parent Information',
       },
@@ -133,7 +137,6 @@ export class ApprovedPatientReportsComponent implements OnInit {
     ],
     hideeditbutton: true,// all these button options are optional not mandatory
     hidedeletebutton: true,
-    hidedeletemany: true,
     hidestatustogglebutton: true,
     hideviewbutton: true,
     tableheaders: [
@@ -144,7 +147,7 @@ export class ApprovedPatientReportsComponent implements OnInit {
       "cpt_addl",
       "general_details",
     ]
-  }
+  };
   public allUserData_modify_header: any = {
     "general_details": "Related Info",
     "patient_name": "Patient Name",
@@ -173,36 +176,33 @@ export class ApprovedPatientReportsComponent implements OnInit {
 
   public previewModal_detail_skip: any = ['_id', 'user_type', 'status', 'password', 'created_at'];
 
+  public cptcodes: any = [
+    { val: "95923", 'name': '95923' },
+    { val: "95943", 'name': '95943' },
+    { val: "95921", 'name': "95921" },
+    { val: "93923", 'name': "93923" },
+    { val: "93922", 'name': "93922" }
+  ];
   public status: any = [
-    { val: 1, 'name': 'Active' }, 
+    { val: 1, 'name': 'Active' },
     { val: 0, 'name': 'Inactive' }
   ];
   public parent_type: any = [
-    { val: "admin", 'name': 'Admin' }, 
-    { val: "diagnostic_admin", 'name': 'Diagnostic Admin' }, 
-    { val: "distributors", 'name': 'Distributor' }, 
-    { val: "doctor_group", 'name': 'Doctor Group' },
-  ];
-  public cptcodes: any = [
-    { val: "95923", 'name': '95923' }, 
-    { val: "95943", 'name': '95943' }, 
-    { val: "95921", 'name': "95921" }, 
-    { val: "93923", 'name': "93923" }, 
-    { val: "93922", 'name': "93922" }
+    { val: "admin", 'name': 'Admin' },
+    { val: "diagnostic_admin", 'name': 'Diagnostic Admin' },
+    { val: "distributors", 'name': 'Distributor' },
+    { val: "doctor_group", 'name': 'Doctor Group' }
   ];
   public report_type: any = [
-    { val: "RM-3A", 'name': 'RM-3A' }, 
-    { val: "TM FLOW V3", 'name': 'TM FLOW V3' }, 
+    { val: "RM-3A", 'name': 'RM-3A' },
+    { val: "TM FLOW V3", 'name': 'TM FLOW V3' },
     { val: "TM FLOW V4", 'name': 'TM FLOW V4' },
     { val: "CMAT with BP Cuffs", 'name': 'CMAT with BP Cuffs' }
   ];
   public report: any = [
-    { val: 11, 'name': 'Biller Admin Approved' },
-    { val: 12, 'name': 'Biller Admin Not Approved' },
-    { val: 13, 'name': "Biller Admin Hold" },
-    { val: 14, 'name': "Doctor Sign" },
-    { val: 15, "name": "Send to Biller" },
-    { val: 16, "name": "Report Downloaded" },
+    { val: "Biller Admin Approved", 'name': 'Biller Admin Approved' },
+    { val: "Biller Admin Not Approved", 'name': 'Biller Admin Not Approved' },
+    { val: "Biller Admin Hold", 'name': "Biller Admin Hold" }
   ];
   public SearchingEndpoint: any = "datalist";
   public authval: any = [];
@@ -217,7 +217,6 @@ export class ApprovedPatientReportsComponent implements OnInit {
   public search_settings: any = {
     selectsearch: [
       { label: 'Search By Report Type', field: 'report_file_type', values: this.report_type },
-      { label: 'Search By Status', field: 'status', values: this.report },
       { label: 'Search By Parent Type', field: 'parent_type', values: this.parent_type },
       { label: 'Search By CPT Codes', field: 'cpt_codes_search', values: this.cptcodes },
       { label: "Search By Doctor", field: 'doc_name_search', values: this.authval },
@@ -232,6 +231,9 @@ export class ApprovedPatientReportsComponent implements OnInit {
     datesearch: [
       { startdatelabel: "Start Date", enddatelabel: "End Date", submit: "Search", field: "created_at_datetime" }
     ],
+    textsearch: [
+      { label: "Search By Patient Name", field: 'patient_name_search' }
+    ]
   };
   // lib list end
 
@@ -243,48 +245,12 @@ export class ApprovedPatientReportsComponent implements OnInit {
 
     /* Get Auth Token */
     this.jwtToken = cookieService.get('jwtToken');
-
-    /* Get resolve data */
-    this.activatedRoute.data.subscribe(resolveData => {
-      this.allResolveData = resolveData.dataCount.data.dashboardCount[0];
-      //this.viewReportProcessData(this.htmlText.headerText);
-    });
-
-
-    // lib list
-    let endpoint = 'getPatientlistdata-approved';
-    let endpointc = 'getPatientlistdata-approved-count';
-
-    
-    let data: any = {
-      "condition": {
-        "limit": 10,
-        "skip": 0
-      },
-      sort: {
-        "type": 'desc',
-        "field": 'patient_name'
-      }
-    }
-
-    this.http.httpViaPostbyApi1(endpointc, data).subscribe((res: any) => {
-      this.billerData_count = res.count;
-    }, error => {
-      console.log('Oooops!');
-    });
-
-    this.http.httpViaPostbyApi1(endpoint, data).subscribe((res: any) => {
-      // console.log(res);
-      this.allBillerData = res.results.res;
-    }, error => {
-      console.log('Oooops!');
-    });
   }
 
   ngOnInit() {
     let data: any = {
       "source": "patient_data_desc_patient_name",
-      "condition": { "status": { $gt: 10 } },
+      "condition": { "status": { $gt: 10 }, job_tickets_details: { $exists: true } },
       "token": this.jwtToken
     }
     this.http.httpViaPost("datalist", data).subscribe((response: any) => {
@@ -429,8 +395,68 @@ export class ApprovedPatientReportsComponent implements OnInit {
   ngAfterViewInit() {
   }
 
-  viewReportProcessData(flag) {
-    console.log(flag);
+  viewReportProcessData(flag: string = "") {
+    this.allBillerData = [];
+    
+    // lib list
+    let endpoint = 'getPatientlistdata';
+    let endpointc = 'getPatientlistdata-count';
+    let data: any = {
+      "condition": {
+        "limit": 10,
+        "skip": 0
+      },
+      sort: {
+        "type": 'desc',
+        "field": 'patient_name'
+      },
+      "searchcondition": {
+      }
+    }
+
+    switch (flag) {
+      case 'Total Number of Reports Added':
+        break;
+      case 'Total Number of Report Processed':
+        data.status = {
+          "$gte": 8
+        };
+        break;
+      case 'Total Number of Report Signed':
+        data.status = { $eq: 14 };
+        break;
+      case 'Sent to Biller':
+        data.status = {
+          "$eq": 15
+        };
+        break;
+      case 'Reports Downloaded':
+        data.status = {
+          "$eq": 16
+        };
+        break;
+      case 'Reports Pending Sing':
+        data.status = {
+          "$eq": 11
+        };
+        break;
+      case 'Job Tickets':
+        document.getElementById("jobTickets").scrollIntoView();
+        break;
+    }
+
+    this.http.httpViaPost(endpointc, data).subscribe((res: any) => {
+      this.billerData_count = res.count;
+    }, error => {
+      console.log('Oooops!');
+    });
+
+    this.http.httpViaPost(endpoint, data).subscribe((res: any) => {
+      this.allBillerData = res.results.res;
+    }, error => {
+      console.log('Oooops!');
+    });
   }
 
 }
+
